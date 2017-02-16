376658e5525128eeea3bc33a7f43dfba96699a30
Ilan Cohen <ilanco@gmail.com>
2017-02-16 16:10:56 -0600

    release version 0.0.2

--- wordpress-openid-connect.php
+++ wordpress-openid-connect.php
@@ -5,7 +5,7 @@
  * Plugin Slug: wordpress-openid-connect
  * Plugin URI: https://
  * Description: This plugin allows user authentication and registration using an openid connect server
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Ilan Cohen
  * Author Email: ilanco@gmail.com
  * Author URI: https://github.com/ilanco
